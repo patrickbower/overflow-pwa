90001c2b809eac4b97b73d0e43507cdc04990a3e
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-07-03 18:06:25 +0100

    feat(api): initial Trello api call
    Returns all cards with filters applied to access corresponding lists,
    labels and ids.

---
+++ src/utils/config.js
@@ -0,0 +1,8 @@
+const config = {
+  url: "https://api.trello.com",
+  key: "bb6807f13b020310a0543a81ebf10765",
+  token: "d89724c1f1285f66151e76c547600c779272f3df7cb7124dabe1f421324bd42c",
+  boardId: "595a5877c9d2d6e30c17c32f"
+};
+
+export default config;
