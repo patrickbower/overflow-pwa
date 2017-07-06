56c90b6bf3c15caeb78b11d8a532734bd94d5207
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-07-06 11:24:10 +0100

    feat(middleware): add listName API

--- src/utils/trello.js
+++ src/utils/trello.js
@@ -1,18 +1,18 @@
 import config from "./config";
 
 export const get = {
-  all: `/1/boards/${config.boardId}/lists?cards=open&card_fields=name&fields=name&card_fields=labels&fields=labels`
+  all: `/1/boards/${config.boardId}/lists?`
 };
 
 export function query(action) {
   return `${config.url}${action}&key=${config.key}&token=${config.token}`;
 }
 
-export function request(url, callBack) {
-  fetch(url)
+export function request(url) {
+  return fetch(query(url))
     .then(response => response.json())
     .then(data => {
-      callBack(data);
+      return data;
     })
     .catch(function(err) {
       throw new Error(err);
