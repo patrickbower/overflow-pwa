fd6eb47ccfb8f3290a62c2292920e4166e2d9400
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-07-11 15:43:33 +0100

    feat(list): break card components into into list
    Break out allows Card component to self state manage.

---
+++ src/componant/List/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import styles from './index.css';
+
+import Card from '../../componant/Card';
+import * as middleware from '../../middleware/listName';
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lists: []
+    };
+  }
+
+  async getData() {
+    const lists = await middleware.listName();
+    this.setState({ lists: lists });
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    if (this.state.lists.length) {
+      return (
+        <div className={styles.container}>
+          {this.state.lists.map(list => <Card list={list} key={list.id} />)}
+        </div>
+      );
+    } else {
+      return <p>loading</p>;
+    }
+  }
+}
+
+export default List;
