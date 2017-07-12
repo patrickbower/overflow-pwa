8fdcc0954229128190ed1f3607fba408467d2624
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-07-12 11:17:32 +0100

    feat(cardbody): map middleware todo data to component

--- src/componant/CardBody/index.js
+++ src/componant/CardBody/index.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import styles from './index.css';
 
 class CardBody extends Component {
+  renderListItem(item, index) {
+    return (
+      <li className={styles.item} key={index}>
+        {item.name}
+        <a href="#">
+          <svg className={styles.iconTick}>
+            <use xlinkHref="#icon-tick" />
+          </svg>
+        </a>
+      </li>
+    );
+  }
+
   render() {
+    const { todo } = this.props;
     return (
       <ul className={styles.list}>
-        <li className={styles.item}>
-          Change money to euros
-          <a href="#">
-            <svg className={styles.iconTick}>
-              <use xlinkHref="#icon-tick" />
-            </svg>
-          </a>
-        </li>
-        <li className={styles.item}>Virgin mobile in EU</li>
-        <li className={styles.item}>Book car in France</li>
+        {todo.map((item, index) => this.renderListItem(item, index))}
       </ul>
     );
   }
